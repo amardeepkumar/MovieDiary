32c238502af524498bb2ad3d16ffd0f9d8633d50
vijiaccolite <vijayalakshmi.in@accoliteindia.com>
2017-01-21 15:20:41 +0530

    Fixed click issue on widget

--- app/src/main/java/com/udacity/moviediary/MovieDiaryAppWidgetConfigure.java
+++ app/src/main/java/com/udacity/moviediary/MovieDiaryAppWidgetConfigure.java
@@ -1,10 +1,13 @@
 package com.udacity.moviediary;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.RemoteViews;
+
+import com.udacity.moviediary.activity.MovieActivity;
 
 /**
  * Created by Amardeep Kumar on 1/21/2017.
@@ -30,6 +33,10 @@ public class MovieDiaryAppWidgetConfigure extends Activity {
         RemoteViews views = new RemoteViews(this.getPackageName(),
                 R.layout.appwidget_provider_layout);
         appWidgetManager.updateAppWidget(mAppWidgetId, views);
+        Intent intentToLaunch = new Intent(this, MovieActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intentToLaunch, 0);
+
+        views.setOnClickPendingIntent(R.id.button, pendingIntent);
 
         Intent resultValue = new Intent();
         resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
